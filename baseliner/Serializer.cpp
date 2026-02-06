5930d63fb87b52267dc6244babd1dc7e71abb2b2
Côme Eyraud <come.eyraud@polymtl.ca>
2026-02-06 17:44:25 -0500

    Serializer to file

---
+++ baseliner/Serializer.cpp
@@ -0,0 +1,29 @@
+#include <baseliner/Serializer.hpp>
+#include <fstream>
+#include <iostream>
+
+namespace Baseliner {
+  void result_to_file(std::vector<Result> results, std::string filename) {
+    std::ifstream infile(filename);
+    bool create_new_file = true;
+    bool file_exists = false;
+    if (infile.is_open()) {
+      file_exists = true;
+    }
+    if (file_exists && create_new_file) {
+      std::cout << filename << " already exists, but headers doesnt match. Writing in " << filename << ".new";
+      filename = filename + ".new";
+    }
+    std::ofstream file;
+    if (create_new_file) {
+      file = std::ofstream(filename);
+    } else {
+      file = std::ofstream(filename, std::ios::app);
+    }
+
+    if (!file.is_open()) {
+      throw std::runtime_error("Could not open file: " + filename);
+    }
+    serialize(file, results);
+  }
+} // namespace Baseliner
--- baseliner/Serializer.hpp
+++ baseliner/Serializer.hpp
@@ -1,5 +1,6 @@
 #ifndef BASELINER_SERIALIZER_HPP
 #define BASELINER_SERIALIZER_HPP
+#include <baseliner/Result.hpp>
 #include <ostream>
 
 namespace Baseliner {
@@ -7,5 +8,6 @@ namespace Baseliner {
   template <typename T>
   void serialize(std::ostream &os, const T &obj);
 
+  void result_to_file(std::vector<Result>, std::string filename);
 } // namespace Baseliner
 #endif // BASELINER_SERIALIZER_HPP
