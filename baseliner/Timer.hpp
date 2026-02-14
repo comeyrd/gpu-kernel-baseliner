5c4f5fbc08f0c1b59f0809fec32e9d9be63bea51
Côme Eyraud <come.eyraud@polymtl.ca>
2026-02-13 21:04:36 -0500

    Moving GPUTimer to device

--- baseliner/Timer.hpp
+++ baseliner/Timer.hpp
@@ -10,20 +10,6 @@ namespace Baseliner {
     virtual void measure_stop() = 0;
     virtual auto time_elapsed() -> float_milliseconds = 0;
   };
-  template <typename stream_t>
-  class IGpuTimer {
-  public:
-    virtual ~IGpuTimer() = default;
-    IGpuTimer() = default;
-    IGpuTimer(const IGpuTimer &) = delete;
-    auto operator=(const IGpuTimer &) -> IGpuTimer & = delete;
-    IGpuTimer(IGpuTimer &&) = delete;
-    auto operator=(IGpuTimer &&) -> IGpuTimer & = delete;
 
-  protected:
-    virtual void measure_start(std::shared_ptr<stream_t> stream) = 0;
-    virtual void measure_stop(std::shared_ptr<stream_t> stream) = 0;
-    virtual auto time_elapsed() -> float_milliseconds = 0;
-  };
 } // namespace Baseliner
 #endif // ITIMER_HPP
