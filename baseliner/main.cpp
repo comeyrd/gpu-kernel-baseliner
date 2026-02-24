7d489991f6251890868b7be40bdcff67e803b2e9
Côme Eyraud <come.eyraud@polymtl.ca>
2026-02-24 11:24:48 -0500

    Updating main to use the new concepts

--- baseliner/main.cpp
+++ baseliner/main.cpp
@@ -1,9 +1,13 @@
 
-#include <baseliner/Manager.hpp>
+#include <baseliner/Recipe.hpp>
 #include <baseliner/Result.hpp>
 #include <baseliner/Serializer.hpp>
 #include <baseliner/Suite.hpp>
-#include <baseliner/Task.hpp>
+#include <baseliner/managers/BackendManager.hpp>
+#include <baseliner/managers/BenchmarkCaseManager.hpp>
+#include <baseliner/managers/StoppingManager.hpp>
+#include <baseliner/managers/SuiteManager.hpp>
+
 #include <baseliner/backend/cuda/CudaBackend.hpp>
 #include <baseliner/stats/StatsDictionnary.hpp>
 #include <chrono>
@@ -29,60 +33,78 @@ static auto generate_uid() -> std::string { // NOLINT
   return stringstream.str();
 };
 
-__attribute__((weak)) auto main(int argc, char **argv) -> int { // NOLINT
-  // std::cout << argc << argv[0] << std::endl;
-  std::cout << "Baseliner" << "\n";
-  auto *manager = TaskManager::instance();
-  std::string backend;
-  std::string selected_benchmark;
-  std::string selected_case;
-  std::string added_stat;
-  std::cout << "Backends \n";
-  for (auto name : BackendRegistry::list_backends()) {
-    std::cout << name << "\n";
-    backend = name;
+static void list_things() {
+  auto backends = BackendManager::list_backends();
+  std::cout << "Number of available Backends : " << backends.size() << "\n ";
+  for (auto backend_name : backends) {
+    std::cout << backend_name << "\n";
+    auto *device_manager = BackendManager::get(backend_name);
+    std::cout << "Cases : \n";
+    for (auto name : device_manager->list_cases()) {
+      std::cout << name << "\n";
+    }
+    std::cout << "Benchmarks \n";
+    for (auto name : device_manager->list_benchmarks()) {
+      std::cout << name << "\n";
+    }
   }
-  auto device_manager = BackendRegistry::get(backend);
-  std::cout << "Cases : \n";
-  for (auto name : device_manager->list_cases()) {
+  std::cout << "------------------\n";
+  std::cout << "Suites \n";
+  for (auto name : SuiteManager::list_suites()) {
     std::cout << name << "\n";
-    selected_case = name;
-  }
-  std::cout << "Benchmarks \n";
-  for (auto name : device_manager->list_benchmarks()) {
-    std::cout << name << "\n";
-    selected_benchmark = name;
   }
   auto *stat_dict = Stats::StatsDictionnary::instance();
-  std::cout << "Stats \n";
+  std::cout << "Available Stats :\n";
   for (auto name : stat_dict->list_stats()) {
     std::cout << name << "\n";
-    added_stat = name;
+  }
+  auto *s_manager = StoppingManager::instance();
+  std::cout << "Available Stopping Criterions :\n";
+  for (auto name : s_manager->list_stopping()) {
+    std::cout << name << "\n";
   }
 
-  auto bench = device_manager->get_benchmark_with_case(selected_benchmark, selected_case);
-  // bench->add_stats({"Median", "SortedExecutionTimeVector", "Q1"});
-  BASELINER_REGISTER_TASK(bench);
-  const auto &list = manager->get_tasks();
-
-  std::cout << "[Baseliner] Total Registered Executables: " << list.size() << "\n";
-
-  if (list.empty()) {
-    std::cout << "Warning: No kernels were registered. Check linker settings." << "\n";
-    return 0;
+  std::cout << "Recipes : \n";
+  for (auto &recipe : RecipeManager::get_recipes()) {
+    std::cout << "backend : " << recipe.m_backend;
+    std::cout << "case : " << recipe.m_case;
+    std::cout << "benchmark : " << recipe.m_benchmak;
+    if (recipe.m_suite.has_value()) {
+      std::cout << "suite : " << recipe.m_suite.value();
+    }
   }
-  std::vector<Result> results_vector;
-  for (const auto &exe : list) {
-    std::vector<Result> local_results = exe->run_all();
-    std::cout << exe->print_console(local_results);
-    // reserve space to avoid multiple reallocations
-    results_vector.reserve(results_vector.size() + local_results.size());
+};
 
-    for (auto &res : local_results) {
-      results_vector.emplace_back(std::move(res));
+__attribute__((weak)) auto main(int argc, char **argv) -> int { // NOLINT
+  std::cout << "Baseliner" << "\n";
+  auto recipes = RecipeManager::get_recipes();
+  std::cout << "[Baseliner] Total Registered Recipes: " << recipes.size() << "\n";
+  std::vector<Result> results;
+  if (recipes.empty()) {
+    list_things();
+  }
+  for (auto &recipe : RecipeManager::get_recipes()) {
+    auto *backend = BackendManager::get(recipe.m_backend);
+    auto benchmark = backend->get_benchmark_with_case(recipe.m_benchmak, recipe.m_case);
+    auto bench_w_stopping = [benchmark, recipe]() -> std::shared_ptr<IBenchmark> {
+      auto bench = benchmark();
+      auto func = StoppingManager::instance()->get_stopping_recipe(recipe.m_stopping);
+      bench->set_stopping_criterion(func);
+      return bench;
+    };
+    if (recipe.m_suite.has_value()) {
+      auto suite = std::make_shared<SingleAxeSuite>();
+      suite->set_benchmark(bench_w_stopping);
+      suite->set_axe(SuiteManager::get_suite(recipe.m_suite.value()));
+      auto temp_res = suite->run_all();
+      results.insert(results.end(), temp_res.begin(), temp_res.end());
+    } else {
+      auto bench = bench_w_stopping();
+      results.push_back(bench->run());
     }
   }
   const std::string filename = generate_uid() + ".json";
-  result_to_file(results_vector, filename);
+
+  result_to_file(results, filename);
   return 0;
 };
