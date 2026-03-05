4eec95911bec943b81198eb0dc7931a8fbdb6a39
Côme Eyraud <come.eyraud@polymtl.ca>
2026-03-05 15:15:42 -0500

    Bumping to version 0.9.0

--- baseliner/Version.hpp
+++ baseliner/Version.hpp
@@ -3,9 +3,9 @@
 #include <string_view>
 namespace Baseliner::Version {
   inline constexpr int major = 0; // NOLINT
-  inline constexpr int minor = 8; // NOLINT
+  inline constexpr int minor = 9; // NOLINT
   inline constexpr int patch = 0; // NOLINT
-  inline constexpr std::string_view string_view = "0.8.0";
+  inline constexpr std::string_view string_view = "0.9.0";
   inline auto string() -> std::string {
     return std::string(string_view);
   }
