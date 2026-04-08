3904b753703c88b3f593f7303c66add44ae02d28
Côme Eyraud <come.eyraud@polymtl.ca>
2026-04-08 10:34:17 -0400

    Having a header with all core necessary headers

---
+++ baseliner/Core.hpp
@@ -0,0 +1,8 @@
+#ifndef BASELINER_CORE_HPP
+#define BASELINER_CORE_HPP
+
+#include <baseliner/core/Benchmark.hpp>
+#include <baseliner/core/Case.hpp>
+#include <baseliner/core/StoppingCriterion.hpp>
+
+#endif // BASELINER_CORE_HPP
