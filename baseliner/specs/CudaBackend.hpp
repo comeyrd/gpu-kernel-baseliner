c235974254bff7a27852fe944c21a0e69fcfe6bf
Côme Eyraud <come.eyraud@polymtl.ca>
2026-04-27 15:46:07 -0400

    Fixing StatsEngine and CHECK_BACKEND

--- baseliner/specs/CudaBackend.hpp
+++ baseliner/specs/CudaBackend.hpp
@@ -7,6 +7,21 @@
 namespace Baseliner::Hardware {
   using CudaBackend = Backend<cudaStream_t, std::monostate>;
 } // namespace Baseliner::Hardware
+
+inline void check_cuda_error(cudaError_t error_code, const char *file, int line) {
+  if (error_code != cudaSuccess) {
+    throw Baseliner::Errors::hardware_error("CUDA", cudaGetErrorString(error_code), file, line);
+  }
+}
+inline void check_cuda_error_no_except(cudaError_t error_code, const char *file, int line) {
+  if (error_code != cudaSuccess) {
+    auto msg = Baseliner::Errors::hardware_error_noexcept("CUDA", cudaGetErrorString(error_code), file, line);
+    std::cerr << msg << std::endl;
+  }
+}
+#define CHECK_CUDA(error) check_cuda_error(error, __FILE__, __LINE__)                     // NOLINT
+#define CHECK_CUDA_NO_EXCEPT(error) check_cuda_error_no_except(error, __FILE__, __LINE__) // NOLINT
+
 #endif
 
 #endif
--- baseliner/specs/stubs/StatsEngineStub.hpp
+++ baseliner/specs/stubs/StatsEngineStub.hpp
@@ -1,16 +1,6 @@
 #ifndef BASELINER_SPECS_STUBS_STATSENGINESTUB_HPP
 #define BASELINER_SPECS_STUBS_STATSENGINESTUB_HPP
-#include <any>
 #include <baseliner/specs/stubs/StatsStubs.hpp>
-#include <iostream>
-#include <memory>
-#include <numeric>
-#include <optional>
-#include <string>
-#include <typeindex>
-#include <unordered_map>
-#include <variant>
-#include <vector>
 
 namespace Baseliner::Stats {
   class StatsEngine {
@@ -41,36 +31,36 @@ namespace Baseliner::Stats {
     void compute() {
       ArithmeticIntensity{}.calculate(m_intensity, m_bytes, m_flops);
       DataThroughput{}.calculate(m_data_bw, m_time_ms, m_bytes);
-      FLOPTroughput{}.calculate(m_flop_bw, m_time_ms, m_flops);
+      FLOPThroughput{}.calculate(m_flop_bw, m_time_ms, m_flops);
     }
 
     template <typename T>
     [[nodiscard]] auto get() const -> typename T::type {
-      if constexpr (std::is_same_v<T, ByteNumbers>)
+      if constexpr (std::is_same_v<T, ByteNumbers>) {
         return m_bytes;
-      else if constexpr (std::is_same_v<T, FLOPCount>)
+      } else if constexpr (std::is_same_v<T, FLOPCount>) {
         return m_flops;
-      else if constexpr (std::is_same_v<T, ExecutionTime>)
+      } else if constexpr (std::is_same_v<T, ExecutionTime>) {
         return m_time_ms;
-      else if constexpr (std::is_same_v<T, ArithmeticIntensity>)
+      } else if constexpr (std::is_same_v<T, ArithmeticIntensity>) {
         return m_intensity;
-      else if constexpr (std::is_same_v<T, DataThroughput>)
+      } else if constexpr (std::is_same_v<T, DataThroughput>) {
         return m_data_bw;
-      else if constexpr (std::is_same_v<T, FLOPTroughput>)
+      } else if constexpr (std::is_same_v<T, FLOPThroughput>) {
         return m_flop_bw;
-      else
+      } else {
         return typename T::type{};
+      }
     }
 
   private:
     size_t m_bytes = 0;
     size_t m_flops = 0;
-    float m_time_ms = 0.0f;
+    float m_time_ms = 0.0F;
 
-    // Stockage des calculs
-    float m_intensity = 0.0f;
-    float m_data_bw = 0.0f;
-    float m_flop_bw = 0.0f;
+    float m_intensity = 0.0F;
+    float m_data_bw = 0.0F;
+    float m_flop_bw = 0.0F;
   };
 
 } // namespace Baseliner::Stats
