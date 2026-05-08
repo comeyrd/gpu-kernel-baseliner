a85e41fd4875291f1cca350e7041a1a6988b2cd3
EYRAUD Come <ce.eyraud@gmail.com>
2026-05-07 21:36:30 -0400

    trying something

--- baseliner/registry/Register.cpp
+++ baseliner/registry/Register.cpp
@@ -24,6 +24,7 @@ namespace Baseliner {
     BASELINER_REGISTER_STAT(MedianAbsoluteDeviation);
     BASELINER_REGISTER_STAT(Mean);
     BASELINER_REGISTER_STAT(HarmonicMean);
+    BASELINER_REGISTER_STAT(CoefficientOfVariation);
     const std::vector<std::string> default_stats = {"Median", "Mean", "CoefficientOfVariation"};
     BASELINER_REGISTER_DEFAULT_STATS(default_stats);
   } // namespace Stats
